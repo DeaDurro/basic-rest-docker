a8386dda176b5e0d0555eea64cbc32b2cdc43662
dea.duro.dd@gmail.com <103927498+DeaDurro@users.noreply.github.com>
2024-06-14 14:56:18 +0200

    fixed some changes in the compose file and bash script

--- call-test-rest.sh
+++ call-test-rest.sh
@@ -3,21 +3,14 @@
 
 # Load environment variables from .env file
 if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+    source .env
 fi
 
-# Make a GET request to the helloWorld endpoint and store the response
-RESPONSE=$(curl -s http://localhost:${TEST_WEBSERVICE_PORT:-8080}/helloworld)
-
-# Print the response
-echo "Response Body: $RESPONSE"
 
 
 
-# Make a GET request to the helloWorld endpoint and store the response
-#RESPONSE=$(curl -s http://localhost:${TEST_WEBSERVICE_PORT:-8085}/helloworld)
-# Print the response
-#echo "Response Body: $RESPONSE"
+
+
 
 
 
