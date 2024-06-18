d15be28a22af538a25c22ddf028467d8f44eedc8
dea.duro.dd@gmail.com <103927498+DeaDurro@users.noreply.github.com>
2024-06-18 18:01:33 +0200

    latest changes

--- src/main/java/com/example/basicrestanddocker/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/basicrestanddocker/demo/controller/HelloWorldController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/helloworld")
 public final class HelloWorldController {
+    /**
+     * Returns a HelloWorld object.
+     *
+     * @return a HelloWorld object with id and message.
+     */
 
     @GetMapping
     public HelloWorld getHelloWorld() {
