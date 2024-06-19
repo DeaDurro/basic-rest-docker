0e1a582aefcd1edcf2d125d1ed82e951f4914fa5
dea.duro.dd@gmail.com <103927498+DeaDurro@users.noreply.github.com>
2024-06-19 12:07:29 +0200

    new changes

--- call-test-rest.sh
+++ call-test-rest.sh
@@ -5,9 +5,11 @@
 if [ -f .env ]; then
     source .env
 fi
+# Make a GET request to the helloWorld endpoint and store the response
+RESPONSE=$(curl -s http://localhost:${TEST_WEBSERVICE_PORT:-8080}/helloworld)
 
-
-
+# Print the response
+echo "Response Body: $RESPONSE"
 
 
 
